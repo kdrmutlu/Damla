9b2f3acbe04335baed7c84fa2c0d58602c555473
Kadir Mutlu <kadirmutluu@gmail.com>
2016-06-24 00:29:20 +0300

    Arkaplan rengi verildi.

--- core/src/com/kmutlu/damla/DamlaOyunu.java
+++ core/src/com/kmutlu/damla/DamlaOyunu.java
@@ -1,27 +1,40 @@
 package com.kmutlu.damla;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class DamlaOyunu extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class DamlaOyunu implements ApplicationListener{
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		//Ekranı boyar.
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
 	}
 }
