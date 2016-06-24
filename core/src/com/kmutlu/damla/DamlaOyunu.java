9a0dda3b1722e8d4549d352728c409b7ea360dff
Kadir Mutlu <kadirmutluu@gmail.com>
2016-06-24 14:17:08 +0300

    Kova eklendi.

--- core/src/com/kmutlu/damla/DamlaOyunu.java
+++ core/src/com/kmutlu/damla/DamlaOyunu.java
@@ -3,12 +3,28 @@ package com.kmutlu.damla;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class DamlaOyunu implements ApplicationListener{
+
+	private OrthographicCamera kamera;
+	private SpriteBatch batch;
+	private Texture rsmKova;
 
 	@Override
 	public void create() {
 
+		//kamera
+		kamera = new OrthographicCamera();
+		kamera.setToOrtho(false, 800, 480);
+
+		//resimler
+		rsmKova = new Texture(Gdx.files.internal("kova.png"));
+
+		//SpriteBatch
+		batch = new SpriteBatch();
 	}
 
 	@Override
@@ -17,6 +33,17 @@ public class DamlaOyunu implements ApplicationListener{
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		//kamera güncelle
+		kamera.update();
+
+		//sayfa kamera ayar
+		batch.setProjectionMatrix(kamera.combined);
+
+		batch.begin();
+
+		batch.draw(rsmKova, 400, 20);
+
+		batch.end();
 	}
 
 	@Override
